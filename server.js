678ead23d8f902cdc4ed546f188a382f6ea14fc9
wal faizy <faizy.wali@gmail.com>
2017-02-07 23:44:30 +0400

    repo for heroku

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
-var express = require("express");
+var express = require('express');
 
-//make an app
+// Create our app
 var app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static("public"));
+app.use(function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] === 'http') {
+        next();
+    } else {
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
 
-app.listen(3000, function() {
-    console.log("Express server is running");
-})
+app.use(express.static('public'));
+
+app.listen(PORT, function () {
+    console.log('Express server is up on port ' + PORT);
+});
