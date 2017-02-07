2d1b5d07103ad1f42a67c58a162f03e4da5369c1
wal faizy <faizy.wali@gmail.com>
2017-02-08 02:49:04 +0400

    this is commit for about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -11,7 +11,10 @@ var React = require("react");
 
 var About = (props) => {
     return (
+        <div>
         <h4>About Component</h4>
+        <p>Welcome to the about page</p>
+        </div>
     );
 };
 module.exports = About;
