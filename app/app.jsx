5c4207990e2248d5f1b7e19b0f80d86f9c060a2e
wal faizy <faizy.wali@gmail.com>
2017-02-09 13:25:41 +0400

    SCSS added

--- app/app.jsx
+++ app/app.jsx
@@ -11,7 +11,7 @@ require("style!css!foundation-sites/dist/foundation.min.css")
 $(document).foundation();
 
 //Load app css
-require("style!css!applicationStyles")
+require("style!css!sass!applicationStyles")
 
 
 ReactDOM.render(
