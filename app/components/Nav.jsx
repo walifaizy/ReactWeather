bfc9f8c028c6a876752d49bd719e8c84f8dc807d
wal faizy <faizy.wali@gmail.com>
2017-02-10 01:16:31 +0400

    debug the weather form

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,7 +4,13 @@ var{Link,IndexLink} = require("react-router");
 var Nav = React.createClass({
     onSearch: function(e) {
         e.preventDefault();
-        alert("Not wire up");
+        var location = this.refs.search.value;
+        var encodedLocation = encodeURIComponent(location);
+        
+        if(location.length > 0) {
+            this.refs.search.value = "";
+            window.location.hash = "#/?location=" + encodedLocation;
+        }
     },
     
     render: function() {
@@ -28,7 +34,7 @@ var Nav = React.createClass({
                <form onSubmit={this.onSearch}>
                    <ul className="menu">
                        <li>
-                           <input type="search" placeholder="Search Weather by city"></input>
+                           <input type="search" placeholder="Search Weather by city" ref="search"></input>
                        </li>
                         <li>
                            <input type="submit" className="button" value="Get Weather"></input>
--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -15,7 +15,9 @@ var Weather = React.createClass({
          
         this.setState({
             isLoading:true,
-            errorMessage: undefined
+            errorMessage: undefined,
+            location: undefined,
+            temp: undefined
         });
          
         openWeatherMap.getTemp(location).then(function(temp) {
@@ -34,6 +36,20 @@ var Weather = React.createClass({
             location: location,
             temp: 23
         });*/
+    },
+    componentDidMount: function() {
+        var location = this.props.location.query.location;
+        if(location && location.length > 0) {
+            this.handleSearch(location);
+            window.location.hash = "#/";
+        }
+    },
+    componentWillReceiveProps: function(newprops) {
+         var location = this.newprops.location.query.location;
+        if(location && location.length > 0) {
+            this.handleSearch(location);
+            window.location.hash = "#/";
+        }
     },
     render: function() {
         var {isLoading, temp, location ,errorMessage} = this.state;
